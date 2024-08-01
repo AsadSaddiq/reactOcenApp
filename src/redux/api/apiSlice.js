fb890ab551696d2dfb9a751a697ed15bf1fd6fbb
Asad Saddiq <asadryk2911@gmail.com>
2024-08-01 23:57:10 +0500

    Update apiSlice.js

--- src/redux/api/apiSlice.js
+++ src/redux/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://127.0.0.1:8000/",
+    baseUrl: "https://ocean-app-ocjj.vercel.app/",
     prepareHeaders: async (headers, { getState, endpoint }) => {
       const token = JSON.parse(localStorage.getItem("auth"));
 
