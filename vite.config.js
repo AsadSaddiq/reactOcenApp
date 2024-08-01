ae1efa994c31a4434bafd1b082d8740ee21f11e4
AsadSaddiq <asadryk2911@gmail.com>
2024-08-01 11:32:55 +0500

    Better Code

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ["@mui/material/styles"],
+  build: {
+    rollupOptions: {
+      external: [
+        "@mui/material/styles",
+        "@mui/icons-material/ArrowDownward",
+        // Add other problematic modules here
+      ],
+    },
   },
 });
